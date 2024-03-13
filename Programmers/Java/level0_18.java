714162ffc14af73fa65ddf1ec4bb2eb27b1c0bc0
yuunseo <dbstj7457@naver.com>
2024-03-13 11:47:17 +0900

    Lv.0 배열 자르기(class import)

--- Programmers/Java/level0_18.java
+++ Programmers/Java/level0_18.java
@@ -1,5 +1,7 @@
 package Programmers.Java;
 
+import java.util.Arrays;
+
 public class level0_18 {
     public int[] solution(int[] numbers, int num1, int num2) {
         int[] answer =  Arrays.copyOfRange(numbers,num1,num2+1);
