447e119e259d623c6431f076e92fa2063d3b5f4b
yuunseo <dbstj7457@naver.com>
2024-03-13 11:16:25 +0900

    Lv.0 아이스 아메리카노

---
+++ Programmers/Java/level0_13.java
@@ -0,0 +1,10 @@
+package Programmers.Java;
+
+public class level0_13 {
+    public int[] solution(int money) {
+        int[] answer = {0,0};
+        answer[0] = money/5500;
+        answer[1]=money%5500;
+        return answer;
+    }
+}
