d06c242602a0d8078626925a9a86899f0f2e5fd0
yuunseo <dbstj7457@naver.com>
2024-03-16 11:49:30 +0900

    Lv.0 특정 문자 제거하기-오류 수정

--- Programmers/Java/level0_20.java
+++ Programmers/Java/level0_20.java
@@ -4,7 +4,7 @@ public class level0_20 {
     public String solution(String my_string, String letter) {
         String answer = "";
         for(int i=0; i<my_string.length(); i++){
-            if(my_string.charAt(i).equals(letter)) continue;
+            if(my_string.charAt(i)==letter.charAt(0)) continue;
             else answer+=my_string.charAt(i);
         }
         return answer;
