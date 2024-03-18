83bba034d4b59b181162bf5cdc5fc1c8a4d8a530
yuunseo <dbstj7457@naver.com>
2024-03-18 10:04:30 +0900

    Lv.0 순서쌍의 개수

---
+++ Programmers/Java/level0_27.java
@@ -0,0 +1,11 @@
+package Programmers.Java;
+
+public class level0_27 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0) answer++;
+        }
+        return answer;
+    }
+}
