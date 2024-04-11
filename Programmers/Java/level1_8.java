657a5e8a3ecaafe08ea81705aef200fed209d29d
yuunseo <96402411+yuunseo@users.noreply.github.com>
2024-04-11 13:48:15 +0900

    Lv.1 [PCCE 기출문제] 9번 / 이웃한 칸 - success

--- Programmers/Java/level1_8.java
+++ Programmers/Java/level1_8.java
@@ -1,32 +1,20 @@
 class Solution {
     public int solution(String[][] board, int h, int w) {
-        int answer = 0;
-        
+                
         // 상 h-1
         // 하 h+1
         // 좌 w-1
         // 우 w+1
-        // 근데 이제 모서리일 때를 고려해야됨. h=0 또는 h=n-1, w=0 또는 w=n-1
         
         String color = board[h][w];
         int n = board.length;
+        int count = 0;
         
-        if(h==0){
-            if(color.equals(board[h+1][w])) answer++;
-        }else if(h==n-1){
-            if(color.equals(board[h-1][w])) answer++;
-        }else{
-            if(color.equals(board[h+1][w])) answer++;
-            if(color.equals(board[h-1][w])) answer++;
-        }
-        if(w==0){
-            if(color.equals(board[h][w+1])) answer++;
-        }else if(w==n-1){
-            if(color.equals(board[h][w-1])) answer++;
-        }else{
-            if(color.equals(board[h][w+1])) answer++;
-            if(color.equals(board[h][w-1])) answer++;
-        }
-        return answer;
+        if(h > 0 && color.equals(board[h-1][w])) count++; // 위쪽
+        if(h < n-1 && color.equals(board[h+1][w])) count++; // 아래쪽
+        if(w > 0 && color.equals(board[h][w-1])) count++; // 왼쪽
+        if(w < n-1 && color.equals(board[h][w+1])) count++; // 오른쪽
+        
+        return count;
     }
 }
