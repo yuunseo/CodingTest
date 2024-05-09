8f80a2c4302aeaa198a0c1e6ef074acaef89fdb4
yuunseo <96402411+yuunseo@users.noreply.github.com>
2024-05-09 12:49:14 +0900

    Lv.1 크레인 인형 뽑기 - success
    인형이 기존에 들어있는 칸도 스택처럼 위>아래인데, 반대로 연산해서 계속 오류나고 있었는데, 이 부분 수정 후 해결!

--- Programmers/Java/level1_10.java
+++ Programmers/Java/level1_10.java
@@ -2,40 +2,32 @@ import java.util.Stack;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        // 인형뽑기 칸 스택
         int answer = 0;
         Stack<Integer> result = new Stack<>();
         
-        for(int num: moves){
-            // num-1 번째 열에서 인형 가져오기
-            int col_num = num-1;
-            int row_num=0;
-            
-            while(row_num<5){
+        for(int i: moves){
+            int num = i-1;
+            for(int j=0; j<board.length; j++){
                 
-                // 인형이 없는 칸이라면, 다음 칸으로 이동
-                if(board[row_num][col_num]==0) 
-                    row_num+=1;
-                // 인형이 있는 칸이라면, 인형을 잡아서 이동
-                else if(board[row_num][col_num]!=0){
-                    // result가 빈 스택이라면, 그대로 push
-                    if(result.empty()){
-                        result.push(board[row_num][col_num]);
-                    }// result의 맨 위에 존재하는 인형이라면, 함께 pop
-                    else if(result.peek() == board[row_num][col_num]){
-                        answer+=1;
-                        result.pop();
-                    }// result의 맨 위에 존재하는 인형이 아니라면, 그대로 push
-                    else{
-                        result.push(board[row_num][col_num]);    
+                // 인형이 있는 칸만 처리하기
+                if(board[j][num]!=0){
+                    // 비어있거나, 비어있지 않은데 이전 인형과 다른 경우에는 push
+                    if(result.empty() || (!result.empty() && result.peek() != board[j][num])){
+                        result.push(board[j][num]);
+                        board[j][num]=0; 
+                        break;
                     }
-                    
-
+                    // 비어있지 않은데 이전 인형과 같은 경우에는 pop
+                    else if(!result.empty() && result.peek() == board[j][num]){
+                        answer+=2;
+                        result.pop();
+                        board[j][num]=0; 
+                        break;
+                    }
                 }
             }
-            
-
         }
+        
         return answer;
     }
 }
