1cc1d975df82189ac50324e9f17dff54e447aee1
yuunseo <96402411+yuunseo@users.noreply.github.com>
2024-05-11 14:40:31 +0900

    [반복문] 25314번. 코딩은 체육과목 입니다.

---
+++ Baekjoon/Java/prob25314.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int count = (int)num/4;
+        for(int i=0; i<count; i++){
+            System.out.print("long ");
+        }
+        System.out.println("int");
+        
+            
+	}
+}
