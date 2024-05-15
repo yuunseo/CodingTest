2cb28683b73f7f482e2821e9f50190e0c9425ba6
yuunseo <96402411+yuunseo@users.noreply.github.com>
2024-05-15 20:55:49 +0900

    Lv.1 [2022 KAKAO TECH INTERNSHIP]성격 유형 검사하기 - refactoring
    반복되는 부분은 for문으로 수정함.

--- Programmers/Java/level1_13.java
+++ Programmers/Java/level1_13.java
@@ -1,7 +1,6 @@
 import java.util.HashMap;
 
 class Solution {
-    // HashMap에 값을 더하는 메소드
     public static void addValue(HashMap<Character, Integer> map, char key, int valueToAdd) {
         if (map.containsKey(key)) {
             map.put(key, map.get(key) + valueToAdd); 
@@ -9,24 +8,21 @@ class Solution {
             map.put(key, valueToAdd); 
         }
     }
-    
+
     public String solution(String[] survey, int[] choices) {
         String answer = "";
+        String[] personality = {"RT","CF","JM","AN"};
         HashMap< Character, Integer> map = new HashMap<>();
-        map.put('R',0);
-        map.put('T',0);
-        map.put('C',0);
-        map.put('F',0);
-        map.put('J',0);
-        map.put('M',0);
-        map.put('A',0);
-        map.put('N',0);
-        
-        
+        for(int i=0; i<personality.length; i++){
+            map.put(personality[i].charAt(0),0);
+            map.put(personality[i].charAt(1),0);
+        }
+
+
         for(int i=0; i<survey.length; i++){
             int score=0;
             char chr='a';
-            
+
             if(choices[i]<4){ // 4 미만일 경우, 성격 유형은 survey.charAt(0) 
                 score = 4-choices[i];
                 chr = survey[i].charAt(0);
@@ -37,14 +33,13 @@ class Solution {
                 addValue(map, chr, score);
             }// 4인 경우, 그대로
         }
-        
-        String[] personality = {"RT","CF","JM","AN"};
+
         for(int i=0; i<personality.length; i++){
             char num1 = personality[i].charAt(0);
             char num2 = personality[i].charAt(1);
             answer += (map.get(num1)>= map.get(num2) ? num1 : num2);
         }
-        
+
         return answer;     
     }
 }
