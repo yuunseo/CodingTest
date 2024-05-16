ead3d5534446971f97b086b55deddc44f344ac24
yuunseo <96402411+yuunseo@users.noreply.github.com>
2024-05-16 09:33:18 +0900

    Lv.0 코드 처리하기 - refactoring
    if문의 나열 대신 if문의 핵심 조건들로만 구성하기.

--- Programmers/Java/level0_74.java
+++ Programmers/Java/level0_74.java
@@ -2,15 +2,14 @@ class Solution {
     public String solution(String code) {
         String answer = "";
         int mode = 0;
+        
         for(int i=0; i<code.length(); i++){
-            if(mode == 0 && code.charAt(i) != '1' && i%2==0){
+            if(code.charAt(i) == '1'){
+                mode = (mode==0)? 1 : 0;
+                continue;
+            }
+            if(mode == i%2){
                 answer += code.charAt(i);
-            }else if(mode == 0 && code.charAt(i) == '1'){
-                mode = 1;
-            }else if(mode == 1 && code.charAt(i) != '1' && i%2!=0){
-                answer += code.charAt(i);
-            }else if(mode == 1 && code.charAt(i) == '1'){
-                mode = 0;
             }
         }
         return (answer.equals("") ? "EMPTY" : answer);
