2d35bfe01aa974b689bd05bdb35517bff221108f
yuunseo <96402411+yuunseo@users.noreply.github.com>
2024-05-24 13:21:21 +0900

    [문자열] 11654번. 아스키 코드

---
+++ Baekjoon/Java/prob11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println((int)(str.charAt(0)));
+
+	}
+}
