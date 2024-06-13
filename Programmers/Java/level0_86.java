0c4088bbf91dd319b3298c2a42663889de6910ae
yuunseo <96402411+yuunseo@users.noreply.github.com>
2024-06-13 11:01:01 +0900

    Lv.0 세로 읽기 - new String 잘못 작성한 부분 수정

--- Programmers/Java/level0_86.java
+++ Programmers/Java/level0_86.java
@@ -1,9 +1,14 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
-        String[] temp = {"","","",""};
+        String[] temp = new String[m];
         int idx = 0;
         int repeat_num = (int)(my_string.length()/m);
         
+        // null 방지 초기화
+        for(int i=0; i<temp.length; i++){
+            temp[i] = "";
+        }
+        // my_string 값 추가
         for(int i=0; i<repeat_num; i++){
             for(int j=0; j<m; j++){
                 temp[j] += my_string.charAt(idx);
